b1a2a79137e5719af0ff68a1aa78ecefcfe51070
Magno Costa <magnocp123@gmail.com>
2022-06-03 14:39:01 -0300

    coreções na data

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,17 +1,19 @@
 class NegociacaoController {
   constructor() {
     let $ = document.querySelector.bind(document)
-
     this._inputData = $('#data')
     this._inputQuantidade = $('#quantidade')
     this._inputValor = $('#valor')
     this._listaNegociacoes = new ListaNegociacoes()
+    this._negociacoesView = new NegociacoesView($('#negociacoesView'))
+
+    this._negociacoesView.update(this._listaNegociacoes)
   }
 
   adiciona(event) {
     event.preventDefault()
-
-    this._listaNegociacoes.adiciona(this._criaNegociacao)
+    this._listaNegociacoes.adiciona(this._criaNegociacao())
+    this._negociacoesView.update(this._listaNegociacoes)
     this._limpaFormulario()
   }
 
@@ -22,11 +24,11 @@ class NegociacaoController {
       this._inputValor.value
     )
   }
+
   _limpaFormulario() {
     this._inputData.value = ''
     this._inputQuantidade.value = 1
     this._inputValor.value = 0.0
-
     this._inputData.focus()
   }
 }
--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -2,6 +2,7 @@ class ListaNegociacoes {
   constructor() {
     this._negociacoes = []
   }
+
   adiciona(negociacao) {
     this._negociacoes.push(negociacao)
   }
