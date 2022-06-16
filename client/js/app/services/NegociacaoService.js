ac60147234e1edcb6605cc731358385946b3c3d3
Magno Costa <magnocp123@gmail.com>
2022-06-16 09:20:50 -0300

    criação do método obterNegociacoes

--- client/js/app/services/NegociacaoService.js
+++ client/js/app/services/NegociacaoService.js
@@ -4,70 +4,88 @@ class NegociacaoService {
   }
 
   obterNegociacoesDaSemana() {
-    return new Promise((resolve, reject) => {
-      this._http
-        .get('negociacoes/semana')
-        .then(negociacoes => {
-          resolve(
-            negociacoes.map(
-              objeto =>
-                new Negociacao(
-                  new Date(objeto.data),
-                  objeto.quantidade,
-                  objeto.valor
-                )
+    return this._http
+      .get('negociacoes/semana')
+      .then(negociacoes => {
+        console.log(negociacoes)
+
+        return negociacoes.map(
+          objeto =>
+            new Negociacao(
+              new Date(objeto.data),
+              objeto.quantidade,
+              objeto.valor
             )
-          )
-        })
-        .catch(erro => {
-          console.log(erro)
-          reject('Não foi possível obter as negociações da semana')
-        })
-    })
+        )
+      })
+      .catch(erro => {
+        console.log(erro)
+        throw new Error('Não foi possível obter as negociações da semana')
+      })
   }
 
   obterNegociacoesDaSemanaAnterior() {
-    return new Promise((resolve, reject) => {
-      this._http
-        .get('negociacoes/anterior')
-        .then(negociacoes => {
-          resolve(
-            negociacoes.map(
-              objeto =>
-                new Negociacao(
-                  new Date(objeto.data),
-                  objeto.quantidade,
-                  objeto.valor
-                )
+    return this._http
+      .get('negociacoes/anterior')
+      .then(negociacoes => {
+        console.log(negociacoes)
+
+        return negociacoes.map(
+          objeto =>
+            new Negociacao(
+              new Date(objeto.data),
+              objeto.quantidade,
+              objeto.valor
             )
-          )
-        })
-        .catch(erro => {
-          console.log(erro)
-          reject('Não foi possível obter as negociações da semana anterior')
-        })
-    })
+        )
+      })
+      .catch(erro => {
+        console.log(erro)
+        throw new Error(
+          'Não foi possível obter as negociações da semana anterior'
+        )
+      })
   }
-  obterNegociacoesDaSemanaRetrasada(cb) {
-    return new Promise((resolve, reject) => {
-      this._http
-        .get('negociacoes/retrasada')
-        .then(negociacoes => {
-          resolve(
-            negociacoes.map(
-              objeto =>
-                new Negociacao(
-                  new Date(objeto.data),
-                  objeto.quantidade,
-                  objeto.valor
-                )
+
+  obterNegociacoesDaSemanaRetrasada() {
+    return this._http
+      .get('negociacoes/retrasada')
+      .then(negociacoes => {
+        console.log(negociacoes)
+
+        return negociacoes.map(
+          objeto =>
+            new Negociacao(
+              new Date(objeto.data),
+              objeto.quantidade,
+              objeto.valor
             )
-          )
-        })
-        .catch(erro => {
-          console.log(erro)
-          reject('Não foi possível obter as negociações da semana retrasada')
-        })
-    })
+        )
+      })
+      .catch(erro => {
+        console.log(erro)
+        throw new Error(
+          'Não foi possível obter as negociações da semana retrasada'
+        )
+      })
+  }
+
+  obterNegociacoes() {
+    return Promise.all([
+      this.obterNegociacoesDaSemana(),
+      this.obterNegociacoesDaSemanaAnterior(),
+      this.obterNegociacoesDaSemanaRetrasada()
+    ])
+      .then(periodos => {
+        let negociacoes = periodos.reduce(
+          (dados, periodo) => dados.concat(periodo),
+          []
+        )
+
+        return negociacoes
+      })
+      .catch(erro => {
+        throw new Error(erro)
+      })
   }
 }
